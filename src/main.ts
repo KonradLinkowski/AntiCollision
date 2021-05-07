d12eda7a17333f32a16dec4363d395a5e9dd3baf
Konrad Linkowski <linkowski.konrad97@gmail.com>
2021-05-07 13:10:40 +0200

    Add zooming possibility

--- src/main.ts
+++ src/main.ts
@@ -11,11 +11,51 @@ $canvas.height = window.innerHeight;
 window.addEventListener('resize', () => {
   $canvas.width = window.innerWidth;
   $canvas.height = window.innerHeight;
-  
 })
 
 const stable_color = new Vector3(0, 255, 0);
 const unstable_color = new Vector3(255, 0, 0);
+
+const minScale = 0.1
+let wheelPos = 0.1 
+let scale = 0.35
+const maxScale = 3
+window.addEventListener('wheel', ({ deltaY }) => {
+  updateScale(-deltaY / 5000)
+})
+
+let initialDistance = null
+
+window.addEventListener('touchstart', ({ touches }) => {
+  if (touches.length > 1) {
+    initialDistance = Math.sqrt(
+      Math.pow(touches[0].clientX - touches[1].clientX, 2)
+      + Math.pow(touches[0].clientY - touches[1].clientY, 2)
+    )
+  }
+})
+
+window.addEventListener('touchend', () => {
+  initialDistance = null
+})
+
+window.addEventListener('touchmove', ({ touches }) => {
+  if (initialDistance !== null) {
+    const distance = Math.sqrt(
+      Math.pow(touches[0].clientX - touches[1].clientX, 2)
+      + Math.pow(touches[0].clientY - touches[1].clientY, 2)
+    )
+    const diff = distance - initialDistance
+    updateScale(diff / 1000)
+  }
+})
+
+function updateScale(pos: number) {
+  wheelPos += pos
+  if (wheelPos < 0) wheelPos = 0
+  if (wheelPos > 1) wheelPos = 1
+  scale = wheelPos * (maxScale - minScale) + minScale
+}
 
 let speedDownFactor = 1;
 let speedDownCounter = 1;
@@ -111,8 +151,9 @@ function animate(delta) {
 
   ctx.clearRect(0, 0, $canvas.width, $canvas.height);
   ctx.save()
-  ctx.translate(-center_of_mass.x + $canvas.width / 2, -center_of_mass.y + $canvas.height / 2)
-
+  ctx.scale(scale, scale)
+  ctx.translate(-center_of_mass.x + $canvas.width * 0.5 * 1 / scale, -center_of_mass.y + $canvas.height * 0.5 * 1 / scale)
+  
   balls.forEach(b => b.draw(ctx))
   ctx.fillStyle = 'purple'
   ctx.beginPath()
